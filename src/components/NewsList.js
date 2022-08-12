1ad5208b293a8c3c6c50471e12bf286833de6d34
GreyWoulf <greywoulf88@outlook.com>
2022-08-12 10:22:51 -0400

    added components folder

---
+++ src/components/NewsList.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+
+const NewsList = () => {
+    const [articles, setArticles] = useState([])
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default NewsList
