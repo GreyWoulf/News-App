8425d695645f0c8801e580b0b33402996fce7cc0
GreyWoulf <greywoulf88@outlook.com>
2022-08-12 11:09:41 -0400

    added newslist component

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const NewsList = () => {
     const [articles, setArticles] = useState([])
+
+    useEffect (() => {
+        const getArticles = async () => {
+            const response = await axios.get('https://newsapi.org/v2/everything?q=Apple&from=2022-08-12&sortBy=popularity&apiKey=f5ca716ab818428785bb086f400d93c3')
+            console.log(response)
+        }
+        getArticles()
+    }, [])
     return (
         <div>
 
