4d41e3f7e428a1f4e94d348f6ee82334dab40e9d
GreyWoulf <greywoulf88@outlook.com>
2022-08-12 14:21:57 -0400

    accessed weather news

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import NewsItem from './NewsItem'
 
 const NewsList = () => {
     const [articles, setArticles] = useState([])
 
     useEffect (() => {
         const getArticles = async () => {
-            const response = await axios.get('https://newsapi.org/v2/everything?q=Apple&from=2022-08-12&sortBy=popularity&apiKey=f5ca716ab818428785bb086f400d93c3')
+            const response = await axios.get('https://newsapi.org/v2/everything?q=weather +severe +extreme +disaster -phd -career&apiKey=f5ca716ab818428785bb086f400d93c3')
             console.log(response)
+            setArticles(response.data.articles)
         }
         getArticles()
     }, [])
     return (
         <div>
-
+            {articles.map(article => {
+                return (
+                    <NewsItem
+                        title={article.title}
+                        description={article.description}
+                        url={article.url}
+                        urlToImage={article.urlToImage}
+                    />
+                )
+            })}
         </div>
     )
 }
